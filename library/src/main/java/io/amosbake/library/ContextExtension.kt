964cdcc6f5e3301b19ad2f9d77d1571a99f65659
Hao_Yan <amosbake@gmail.com>
2017-08-21 20:55:08 +0800

    add Extension

--- library/src/main/java/io/amosbake/library/ContextExtension.kt
+++ library/src/main/java/io/amosbake/library/ContextExtension.kt
@@ -1,8 +1,18 @@
 package io.amosbake.library
 
 import android.content.Context
+import android.app.KeyguardManager
+import android.telecom.TelecomManager
+import android.telephony.TelephonyManager
+
 
 /**
  * Created by Yanhao
  * date: 17/8/21
  */
+fun Context.isLockScreen():Boolean = (getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager).inKeyguardRestrictedInputMode()
+
+fun Context.isCalling():Boolean {
+    val telephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+    return telephonyManager.callState == TelephonyManager.CALL_STATE_OFFHOOK || telephonyManager.callState == TelephonyManager.CALL_STATE_RINGING
+}
